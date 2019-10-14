e0973145e8252cb8b7edd706c5ef0bb7b0507256
That3Percent <that3percent@gmail.com>
2019-10-14 15:01:09 -0500

    Ran rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::mem::transmute;
 use std::cell::{Ref, RefMut};
+use std::mem::transmute;
 
 // TODO: Parameterize output lifetime instead of making it always 'static. This would require GAT
 
@@ -12,60 +12,60 @@ pub unsafe fn extend_lifetime_mut<T: ExtendableLifeMut>(r: T) -> T::OutMut {
 }
 
 pub unsafe trait ExtendableLife {
-	type Out;
+    type Out;
 
-	unsafe fn extend_lifetime(self) -> Self::Out;
+    unsafe fn extend_lifetime(self) -> Self::Out;
 }
 
 pub unsafe trait ExtendableLifeMut {
-	type OutMut;
-	unsafe fn extend_lifetime_mut(self) -> Self::OutMut;
+    type OutMut;
+    unsafe fn extend_lifetime_mut(self) -> Self::OutMut;
 }
 
 unsafe impl<'a, T: ?Sized + 'static> ExtendableLife for &'a T {
-	type Out=&'static T;
-	unsafe fn extend_lifetime(self) -> Self::Out {
-		transmute(self)
-	}
+    type Out = &'static T;
+    unsafe fn extend_lifetime(self) -> Self::Out {
+        transmute(self)
+    }
 }
 
 unsafe impl<'a, T: ?Sized + 'static> ExtendableLifeMut for &'a mut T {
-	type OutMut=&'static mut T;
-	unsafe fn extend_lifetime_mut(self) -> Self::OutMut {
-		transmute(self)
-	}
+    type OutMut = &'static mut T;
+    unsafe fn extend_lifetime_mut(self) -> Self::OutMut {
+        transmute(self)
+    }
 }
 
 unsafe impl<'a, T: ExtendableLife + Sized> ExtendableLife for Option<T> {
-	type Out = Option<T::Out>;
-	unsafe fn extend_lifetime(self) -> Self::Out {
-		match self {
-			None=>None,
-			Some(inner)=>Some(inner.extend_lifetime())
-		}
-	}
+    type Out = Option<T::Out>;
+    unsafe fn extend_lifetime(self) -> Self::Out {
+        match self {
+            None => None,
+            Some(inner) => Some(inner.extend_lifetime()),
+        }
+    }
 }
 
 unsafe impl<'a, T: ExtendableLifeMut + Sized> ExtendableLifeMut for Option<T> {
-	type OutMut = Option<T::OutMut>;
-	unsafe fn extend_lifetime_mut(self) -> Self::OutMut {
-		match self {
-			None=>None,
-			Some(inner)=>Some(inner.extend_lifetime_mut())
-		}
-	}
+    type OutMut = Option<T::OutMut>;
+    unsafe fn extend_lifetime_mut(self) -> Self::OutMut {
+        match self {
+            None => None,
+            Some(inner) => Some(inner.extend_lifetime_mut()),
+        }
+    }
 }
 
 unsafe impl<'a, T: 'static> ExtendableLife for Ref<'a, T> {
-	type Out=Ref<'static, T>;
-	unsafe fn extend_lifetime(self) -> Self::Out {
-		transmute(self)
-	}
+    type Out = Ref<'static, T>;
+    unsafe fn extend_lifetime(self) -> Self::Out {
+        transmute(self)
+    }
 }
 
 unsafe impl<'a, T: 'static> ExtendableLifeMut for RefMut<'a, T> {
-	type OutMut=RefMut<'static, T>;
-	unsafe fn extend_lifetime_mut(self) -> Self::OutMut {
-		transmute(self)
-	}
+    type OutMut = RefMut<'static, T>;
+    unsafe fn extend_lifetime_mut(self) -> Self::OutMut {
+        transmute(self)
+    }
 }
